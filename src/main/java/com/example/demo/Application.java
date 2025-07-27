f18a9219311b0077f15de0f1a30b9fec2f4d6b55
Andrew Marques <andrewmarques@protonmail.com>
2025-07-27 20:38:58 -0300

    ajustes

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Application {
