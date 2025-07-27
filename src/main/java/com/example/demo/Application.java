07279ec4286b019bde50411bc8571491a4bb6d4b
Andrew Marques <andrewmarques@protonmail.com>
2025-07-27 20:53:39 -0300

    Update Application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,8 +3,9 @@ package com.example.demo;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-
+@OpenAPIDefinition(servers = { @Server(url="/")})
 @SpringBootApplication
 public class Application {
 
